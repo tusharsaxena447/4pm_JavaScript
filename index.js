7622e67587e6fd174f8cea06e6ec742a0a9df746
tusharsaxena447 <65817532+tusharsaxena447@users.noreply.github.com>
2024-03-01 02:10:33 +0530

    camel question

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-let num = 18
- if(num < 0){
-    console.log("negative number")
- }
- else if (num == 0){
-    console.log("Zero")
- }
- else{
-    console.log("Positive number")
- }
+// let num = 18
+//  if(num < 0){
+//     console.log("negative number")
+//  }
+//  else if (num == 0){
+//     console.log("Zero")
+//  }
+//  else{
+//     console.log("Positive number")
+//  }
+
+let user = "hello my name is tushar"
+
+
+
+
+function camel(users){
+   let arr = user.split(" ")
+   var arr2 = Array()
+   for (const key of arr) {
+      
+      let camel = key.split("")
+      
+      camel[0] = camel[0].toUpperCase() 
+      let word = camel.join("")
+      
+      
+      arr2.push(word)
+   }
+   return arr2.join(" ")
+}
+console.log(camel(user))
