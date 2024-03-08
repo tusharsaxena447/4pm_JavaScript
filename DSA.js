cd4dc50e9b44665620afc30d14548d01d3203018
tusharsaxena447 <65817532+tusharsaxena447@users.noreply.github.com>
2024-03-08 10:00:08 +0530

    changed

--- DSA.js
+++ DSA.js
@@ -1,16 +1,27 @@
 const prompt = require("prompt-sync")()
 const arr = [60,3,30,45,8,99,47]
 
-let value = parseInt(prompt("Enter value  "))
+// let value = parseInt(prompt("Enter value  "))
 let index = parseInt(prompt("Enter index  "))
 let arr1 = []
 let arr2 = []
-for(let i =0; i<=index; i++){
-     arr1.push(arr[i])
+// for(let i =0; i<=index; i++){
+//      arr1.push(arr[i])
+// }
+// for(let i = index; i<arr.length; i++){
+//     arr2.push(arr[i])
+// }
+// arr1[index] = value
+// let final = [...arr1,...arr2]
+// console.log(final)
+
+for(let i=0; i<index; i++){
+    arr1.push(arr[i])
 }
-for(let i = index; i<arr.length; i++){
+
+for(let i=index+1;i<arr.length;i++){
     arr2.push(arr[i])
 }
-arr1[index] = value
+
 let final = [...arr1,...arr2]
 console.log(final)
