89a27f7891e98d5ad7acd8c336f0a93834e60286
tusharsaxena447 <65817532+tusharsaxena447@users.noreply.github.com>
2024-03-14 17:21:35 +0530

    clock

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ const tree = ()=>{
 // console.log(tree())
 
 // hoisting---------
-console.log(a)
+// console.log(a)
 var a = "tushar"
 // In JavaScript, hoisting refers to the built-in behavior of the language through which declarations of functions, variables, and classes are moved to the top of their scope – all before code execution. In turn, this allows us to use functions, variables, and classes before they are declared.
 // scope 
@@ -148,4 +148,15 @@ const obj = {
    }
 }
 
-console.log(obj.fullname())
+// console.log(obj.fullname())
+const obj1  = {"name":"tushar",
+   "lastname":"saxena"}
+
+// for(let x in obj1){
+//    console.log(x)
+// }
+
+for (const iterator of Object.values(obj1)) {
+   console.log(iterator)
+}
+
