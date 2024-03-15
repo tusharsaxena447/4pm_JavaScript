634170950a5df5d370c85b6eb11644dead2dce5c
tusharsaxena447 <65817532+tusharsaxena447@users.noreply.github.com>
2024-03-15 23:26:22 +0530

    Update index.js

--- interview/index.js
+++ interview/index.js
@@ -1,13 +1,27 @@
 const arr = ["Apple","Banana","litchi","grappes"]
 
-const para = document.createElement("p")
-para.innerHTML = "this is a paragraph"
-document.body.appendChild(para)
+// const para = document.createElement("p")
+// para.innerHTML = "this is a paragraph"
+// document.body.appendChild(para)
 
-const ul = document.createElement("ul")
-arr.map((e)=>{
-    let li = document.createElement("li")
-    li.innerHTML = e
-    ul.appendChild(li)
-})
-document.body.appendChild(ul)
+// const ul = document.createElement("ul")
+// arr.map((e)=>{
+//     let li = document.createElement("li")
+//     li.innerHTML = e
+//     ul.appendChild(li)
+// })
+// document.body.appendChild(ul)
+let str = "The Quick brown Fox Jumps Over The Lazy Dog brown BROWN";
+// let new_arr = arr.slice(0)
+// console.log(new_arr)
+
+// console.log(str.charAt(5)) 
+// let match = str.match("own");
+// console.log(match)
+
+// arr.unshift("mango")
+let str1 = "racecar"
+// console.log(str1.split("").reverse().join("")===str1)
+
+arr.splice(0,0,"anaar")
+console.log(arr)
