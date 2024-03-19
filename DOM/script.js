d258ba185cf4fe47513e776d786b3b3c73c013e1
tusharsaxena447 <65817532+tusharsaxena447@users.noreply.github.com>
2024-03-19 22:17:22 +0530

    new

---
+++ DOM/script.js
@@ -0,0 +1,22 @@
+var a = document.createElement("div")
+a.style.width="100px"
+a.style.height="100px"
+a.style.border="2px solid"
+document.body.appendChild(a)
+var btn = document.createElement("button")
+btn.innerHTML = "ON"
+btn.style.margin = "5px"
+document.body.appendChild(btn)
+let toggle = true
+
+document.querySelector("button").addEventListener("click",()=>{
+   if (toggle){
+    a.style.backgroundColor = "yellow"
+    toggle = false
+    btn.innerHTML = "OFF"
+   }else{
+    a.style.backgroundColor = "white"
+    toggle = true
+    btn.innerHTML = "ON"
+   }
+})
