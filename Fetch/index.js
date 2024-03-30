e4730e7be26c6d4ec36d7bd6aa2eba8c8fe5cb07
tusharsaxena447 <65817532+tusharsaxena447@users.noreply.github.com>
2024-03-30 16:26:09 +0530

    new fetch products

--- Fetch/index.js
+++ Fetch/index.js
@@ -1,17 +1,23 @@
-fetch('./product.json')
-.then(products => products.json())
+fetch('https://dummyjson.com/products')
+.then(data => data.json())
 .then((res) => {
     let alldata = document.querySelector("#data-result")
     let result = ""
-    res.map((e)=>{
+    res.products.map((e)=>{
         result += `
         <tr>
-        <td>${e.name}</td>
+        <td>${e.id}</td>
+        <td>${e.title}</td>
+        <td>${e.description}</td>
         <td>${e.price}</td>
-        <td>${e.inventory}</td>
-        <td>${e.productCode}</td>
+        <td>${e.discountPercentage}</td>
         </tr>
         `
     })
     alldata.innerHTML = result
 })
+
+
+
+// .then(json => console.log(json))
+            
