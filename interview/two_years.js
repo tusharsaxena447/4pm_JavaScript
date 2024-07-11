da0dcb09a9050af9fb59e24e2b0b955003b6b9f1
tusharsaxena447 <65817532+tusharsaxena447@users.noreply.github.com>
2024-07-11 13:34:36 +0530

    changed

--- interview/two_years.js
+++ interview/two_years.js
@@ -20,49 +20,49 @@
 
 // resolve.then(res => console.log(res)).catch(rej => console.log(rej))
 
-// let counter = 0;
-// let centre = []
-// let radiuses = []
-// function circle(x,y) {
-//   let radius = Math.floor(Math.random() * 100 + 10);
-//   // console.log(radius)
-//   radiuses.push(radius/2)
-//   let btn = document.createElement("div");
-//   btn.style.width = `${radius}px`;
-//   btn.style.height = `${radius}px`;
-//   btn.style.border = "2px solid black";
-//   btn.style.borderRadius = "50%";
-//   btn.style.position = "fixed"
-//   btn.style.top = `${y-(radius/2)}px`
-//   btn.style.left = `${x-(radius/2)}px`
-//   document.body.appendChild(btn);
-// }
-// window.addEventListener("click", (e) => {
-//    let x = e.clientX
-//    let y = e.clientY
-//     centre.push(x)
-//     centre.push(y)
-//     counter++;
-//   if (counter === 2 ){
-//     circle(x,y)
+let counter = 0;
+let centre = []
+let radiuses = []
+function circle(x,y) {
+  let radius = Math.floor(Math.random() * 100 + 10);
+  // console.log(radius)
+  radiuses.push(radius/2)
+  let btn = document.createElement("div");
+  btn.style.width = `${radius}px`;
+  btn.style.height = `${radius}px`;
+  btn.style.border = "2px solid black";
+  btn.style.borderRadius = "50%";
+  btn.style.position = "fixed"
+  btn.style.top = `${y-(radius/2)}px`
+  btn.style.left = `${x-(radius/2)}px`
+  document.body.appendChild(btn);
+}
+window.addEventListener("click", (e) => {
+   let x = e.clientX
+   let y = e.clientY
+    centre.push(x)
+    centre.push(y)
+    counter++;
+  if (counter === 2 ){
+    circle(x,y)
 
-//     let distance = Math.sqrt(((centre[3]-centre[1])**2)+((centre[2]-centre[0])**2))
+    let distance = Math.sqrt(((centre[3]-centre[1])**2)+((centre[2]-centre[0])**2))
 
-//     let inter = (radiuses[0]+radiuses[1])-distance
-//     if(inter > 0){
-//       console.log("intersects")
-//     }
-//     centre = []
-//     radiuses = []
-//   }
-//   else if (counter > 2) {
-//     document.body.innerHTML = ""; 
-//     counter = 1;
-//     circle(x,y);
+    let inter = (radiuses[0]+radiuses[1])-distance
+    if(inter > 0){
+      console.log("intersects")
+    }
+    centre = []
+    radiuses = []
+  }
+  else if (counter > 2) {
+    document.body.innerHTML = ""; 
+    counter = 1;
+    circle(x,y);
 
-//   } else {
-//     circle(x,y);
+  } else {
+    circle(x,y);
 
-//   }
-// });
+  }
+});
 
